bd8b3308cb3e08c1f8de3a6326fee80f5519c635
Srinav <archana2006reddy@gmail.com>
2023-12-16 14:26:18 +0530

    update changes

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ function beforesubmit(event){
    if (captchachecked) {
     let outputdate = document.querySelector(".outputdate");
     let inputdate = document.querySelector(".inputdate"); 
-    console.log("inputdate.value", inputdate.value);
+    console.log("inputdate.value", inputdate.value); //string-->date(IN_US)
 
-    let formateDate = new Date(inputdate.value).toLocaleDateString("en-US");
-    
-    outputdate.value = formateDate;
+    let formatteDate = new Date(inputdate.value).toLocaleDateString("en-US");
+    outputdate.value = formatteDate;
   } else {
     alert("Please check the reCAPTCHA box to submit the Lead");
     event.preventDefault();
@@ -17,7 +16,8 @@ function beforesubmit(event){
 function timestamp() {
      var response = document.getElementById("g-recaptcha-response"); 
      if (response == null || response.value.trim() == "") {
-        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+        elems["ts"] = JSON.stringify(new Date().getTime());
         document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
     } 
 } 
